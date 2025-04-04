6b604bbb736192be0a9cc8a4f53b9c5dcdd577a0
ADTSpreadsheet <2531kaeng@gmail.com>
2025-04-04 07:11:40 +0700

    Update ConfirmRegistration.js

--- routes/ConfirmRegistration.js
+++ routes/ConfirmRegistration.js
@@ -1,11 +1,1 @@
-const express = require('express');
-const router = express.Router();
-const confirmRegistrationController = require('../controllers/ConfirmRegistration');
-
-router.post('/verify-refcode', confirmRegistrationController.verifyRefCode);
-router.post('/verify-serialkey', confirmRegistrationController.verifySerialKey);
 router.post('/complete-registration', confirmRegistrationController.completeRegistration);
-router.post('/resend-serialkey', confirmRegistrationController.resendSerialKey);
-router.get('/check-status/:refCode', confirmRegistrationController.checkRegistrationStatus);
-
-module.exports = router;
