b65b48e8f03693301f7cec0a0a7f1fa0c2c6c74a
ADTSpreadsheet <2531kaeng@gmail.com>
2025-04-08 18:37:11 +0700

    Update verifyLicenseRoute.js

--- routes/verifyLicenseRoute.js
+++ routes/verifyLicenseRoute.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const { verifyLicense1, verifyLicense2 } = require('../controllers/VerifyLicenseController');
 
-// ==============================
+// ===========================
 // POST routes
-// ==============================
+// ===========================
 
-// ใช้ฟังก์ชันจาก controllers/VerifyLicenseController.js
-router.post('/verify-license1', verifyLicense1);  // เรียกใช้งานฟังก์ชัน verifyLicense1 จาก controller
-router.post('/verify-license2', verifyLicense2);  // เรียกใช้งานฟังก์ชัน verifyLicense2 จาก controller
+// ใช้ฟังก์ชันจาก controller
+router.post('/verify-license1', verifyLicense1);
+router.post('/verify-license2', verifyLicense2);
 
 module.exports = router;
