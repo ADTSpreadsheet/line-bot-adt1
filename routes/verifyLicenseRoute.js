308b0126174473380979f2bfe5b24e9e674ba906
ADTSpreadsheet <2531kaeng@gmail.com>
2025-04-11 14:50:13 +0700

    Update verifyLicenseRoute.js

--- routes/verifyLicenseRoute.js
+++ routes/verifyLicenseRoute.js
@@ -1,17 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const {
-  verifyLicense1,
-  verifyLicense2,
-  verifyRefCodeAndSerial
-} = require('../controllers/VerifyLicenseController');
+const { verifyLicense1 } = require('../controllers/VerifyLicenseController');
 
 // ===========================
 // POST Routes
 // ===========================
 router.post('/verify-license1', verifyLicense1);
-router.post('/verify-license2', verifyLicense2);
-router.post('/verifyRefCodeAndSerial', verifyRefCodeAndSerial);
 
 module.exports = router;
