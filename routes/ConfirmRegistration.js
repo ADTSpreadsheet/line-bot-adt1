ecabd53d420355c7b532e998db2e570e445e5ca6
ADTSpreadsheet <2531kaeng@gmail.com>
2025-06-04 22:10:56 +0700

    Delete routes/ConfirmRegistration.js

--- routes/ConfirmRegistration.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const confirmRegistrationController = require('../controllers/ConfirmRegistration');
-
-router.post('/complete-registration', confirmRegistrationController.completeRegistration);
-
-module.exports = router;
