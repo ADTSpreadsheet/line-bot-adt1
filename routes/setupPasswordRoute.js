18d5d73b765c81ae1c6504ab17193f2e7cf4baa9
ADTSpreadsheet <2531kaeng@gmail.com>
2025-06-04 22:10:19 +0700

    Delete routes/setupPasswordRoute.js

--- routes/setupPasswordRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { setupPassword } = require('../controllers/setupPasswordController');
-
-// Endpoint: /router/setup-password
-router.post('/setup-password', setupPassword);
-
-module.exports = router;
